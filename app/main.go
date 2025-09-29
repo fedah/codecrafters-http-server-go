45024276f46ad18162ef73821a62a9985303784a
Franco Edah <franco.edah@outlook.com>
2025-09-28 21:10:17 -0700

    stage 4: respond with 200

--- app/main.go
+++ app/main.go
@@ -4,27 +4,81 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
+const (
+	CRLF = "\r\n"
+	// Response Status Lines
+	OK        = "HTTP/1.1 200 OK"
+	NOT_FOUND = "HTTP/1.1 404 Not Found"
+
+	// Response Headers
+	CONTENT_TYPE   = "Content-Type: "
+	CONTENT_LENGTH = "Content-Length: "
+)
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-	
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	fmt.Println("Binding to port 4221")
+	listener, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
-	_, err = l.Accept()
+
+	fmt.Println("Accepting client connection")
+	conn, err := listener.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	fmt.Println("Getting request path")
+	buffer := make([]byte, 1024)
+	_, err = conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading request: ", err.Error())
+		os.Exit(1)
+	}
+
+	var resp string
+	method, path, version := getRequestLine(buffer)
+
+	fmt.Println(method + " " + path + " " + version)
+
+	fmt.Println("Sending response")
+	switch strings.Split(path, "/")[0] {
+	case "":
+		resp = OK + CRLF
+	case "echo":
+		resp = OK + CRLF
+		resp += CONTENT_TYPE + "text/plain" + CRLF
+		if len(path) > 1 {
+			phrase := strings.Split(path, "/")[1]
+			resp += fmt.Sprintf("%s%v%s", CONTENT_LENGTH, len(phrase), CRLF)
+			resp += phrase + CRLF
+			break
+		}
+	default:
+		fmt.Printf("Error path \"%s\" not found", path)
+		resp = NOT_FOUND + CRLF
+	}
+
+	resp += CRLF
+	fmt.Println(resp)
+	conn.Write([]byte(resp))
+}
+
+func getRequestLine(buffer []byte) (method, path, version string) {
+	requestLine := strings.Split(strings.Split(string(buffer), CRLF)[0], " ")
+	method = requestLine[0]
+	path, version = "", ""
+
+	if len(requestLine) < 3 {
+		path = requestLine[1]
+		version = requestLine[2]
+	} else {
+		version = requestLine[1]
+	}
+	return
 }
